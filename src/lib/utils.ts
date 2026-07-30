c27e9cd6bd13089fb8fcc3593df46294fc9fc93a
robinird <robin.rouillard@outlook.fr>
2026-07-30 23:27:25 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -66,7 +66,7 @@ export function calculateBranchAvailability(
   );
 
   const occupied = users.filter(
-    (user) => branchBenchIds.has(user.benchId) && user.isParked
+    (user) => user.benchId && branchBenchIds.has(user.benchId) && user.isParked
   ).length;
 
   return getAvailabilityStatus(occupied, branch.capacity);
